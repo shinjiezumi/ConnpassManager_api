2e7f81798a19357d97316f31240218313452abfa
shinjiezumi <shinjiezumi.work@gmail.com>
2021-07-15 20:25:39 +0900

    Output log to file

--- src/main.go
+++ src/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"connpass-manager/logger"
 )
 
 // APIポート
@@ -23,15 +25,12 @@ func main() {
 	}
 
 	// アクセスログの設定
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format:           "${time_custom} method=${method}, uri=${uri}, status=${status}\n",
-		CustomTimeFormat: "2006-01-02 15:04:05",
-	}))
+	logger.Setup(e)
 	e.Use(middleware.Recover())
 
 	// ROOT
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "ok")
+		return c.String(http.StatusOK, "ok!")
 	})
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", apiPort)))
