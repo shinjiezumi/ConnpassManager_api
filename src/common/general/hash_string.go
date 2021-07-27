90091e595afdc79d8f36457a867aabc4767069b4
shinjiezumi <shinjiezumi.work@gmail.com>
2021-07-27 22:02:57 +0900

    Use salt in generate hash

--- src/common/general/hash_string.go
+++ src/common/general/hash_string.go
@@ -1,8 +1,10 @@
 package general
 
 import (
-	"crypto/sha256"
 	"encoding/base64"
+	"os"
+
+	"golang.org/x/crypto/scrypt"
 )
 
 // HashString ハッシュ文字列
@@ -10,7 +12,22 @@ type HashString string
 
 // NewHashString ハッシュ文字列を生成する
 func NewHashString(str string) HashString {
-	b := sha256.Sum256([]byte(str))
-	h := base64.URLEncoding.EncodeToString(b[:])
+	salt := loadSalt()
+
+	hash, err := scrypt.Key([]byte(str), []byte(salt), 16384, 8, 1, 32)
+	if err != nil {
+		panic(err)
+	}
+
+	h := base64.URLEncoding.EncodeToString(hash[:])
 	return HashString(h)
+}
+
+func loadSalt() string {
+	salt := os.Getenv("HASH_SALT")
+	if salt == "" {
+		panic("HASH_SALT is empty")
+	}
+
+	return salt
 }
